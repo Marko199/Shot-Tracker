4b209ef2e54d9cb19b831044b6f9afbff5da5c16
Greg Brooks <gregbrooks@gregs-mac.local>
2019-01-07 13:51:57 -0500

    Initial Commit

---
+++ Shot Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shot Tracker
+//
+//  Created by Greg Brooks on 2019-01-07.
+//  Copyright © 2019 Greg Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
