df6e19dbb236856f93854c28d5f9b437109cf2a5
Greg Brooks <gregbrooks@gregs-mac.local>
2019-01-17 19:35:02 -0500

    Update to placement of markers

---
+++ Shot Tracker/Marker_Info_Page.swift
@@ -0,0 +1,30 @@
+//
+//  Marker_Info_Page.swift
+//  Shot Tracker
+//
+//  Created by Greg Brooks on 2019-01-17.
+//  Copyright © 2019 MAG Industries. All rights reserved.
+//
+
+import UIKit
+
+class Marker_Info_Page: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
